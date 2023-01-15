b107874e255c3ca4737810ffb82d6bd423ec1462
DavidMoralesAguilarUD <davidtrokten@gmail.com>
2023-01-14 20:15:44 -0500

    Adding DTO´s, Modifing ListResponse, DogRepository

--- app/src/main/java/com/example/dogdex/api/responses/DogListResponse.kt
+++ app/src/main/java/com/example/dogdex/api/responses/DogListResponse.kt
@@ -1,5 +1,6 @@
 package com.example.dogdex.api.responses
 
 import com.example.dogdex.Dog
+import com.example.dogdex.api.dto.DogDTO
 
-class DogListResponse (val dogs: List<Dog>)
+class DogListResponse (val dogs: List<DogDTO>)
