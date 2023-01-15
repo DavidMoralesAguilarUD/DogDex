517f3f81883883475c935a920f030e287b50d84d
DavidMoralesAguilarUD <davidtrokten@gmail.com>
2023-01-15 13:27:58 -0500

    Passings dogs to DogDetailActivity: DogDetailActivity.xml, DogAdapter.kt, DogListActivity.kt

---
+++ app/src/main/java/com/example/dogdex/dogdetail/DogDetailActivity.kt
@@ -0,0 +1,33 @@
+package com.example.dogdex.dogdetail
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.example.dogdex.Dog
+import com.example.dogdex.R
+import com.example.dogdex.databinding.ActivityDogDetailBinding
+import com.example.dogdex.databinding.ActivityDogDetailBindingImpl
+import com.example.dogdex.databinding.ActivityDogListBinding
+
+class DogDetailActivity : AppCompatActivity() {
+
+    companion object{
+        const val DOG_KEY = "Dog"
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivityDogDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val dog = intent?.extras?.getParcelable<Dog>(DOG_KEY)
+        if(dog == null){
+            Toast.makeText(this, R.string.error_showing_dog_not_found, Toast.LENGTH_SHORT).show()
+            finish()
+            return
+        }
+        binding.dogIndex.text = getString(R.string.dog_index_format, dog.index)
+        binding.lifeExpectancy.text = getString(R.string.dog_life_expectancy_format, dog.lifeExpectancy)
+
+        binding.dog = dog
+    }
+}
--- app/src/main/java/com/example/dogdex/doglist/DogListActivity.kt
+++ app/src/main/java/com/example/dogdex/doglist/DogListActivity.kt
@@ -1,5 +1,6 @@
 package com.example.dogdex.doglist
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
@@ -7,6 +8,8 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.dogdex.Dog
 import com.example.dogdex.R
 import com.example.dogdex.databinding.ActivityDogListBinding
+import com.example.dogdex.dogdetail.DogDetailActivity
+import com.example.dogdex.dogdetail.DogDetailActivity.Companion.DOG_KEY
 
 class  DogListActivity : AppCompatActivity() {
 
@@ -19,6 +22,13 @@ class  DogListActivity : AppCompatActivity() {
         recycler.layoutManager = LinearLayoutManager(this)
 
         val adapter = DogAdapter()
+
+        adapter.setOnItemClickListener {
+            // Pasar el dog a DogDetailActivity
+            val intent = Intent(this, DogDetailActivity::class.java)
+            intent.putExtra(DOG_KEY, it)
+            startActivity(intent)
+        }
         recycler.adapter = adapter
 
         dogListViewModel.dogList.observe(this){
