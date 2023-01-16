b454a9cd3d88a7840c44cfde2c6e5b73240e230f
DavidMoralesAguilarUD <davidtrokten@gmail.com>
2023-01-16 16:45:30 -0500

    Generalizing requests

--- app/src/main/java/com/example/dogdex/doglist/DogRepository.kt
+++ app/src/main/java/com/example/dogdex/doglist/DogRepository.kt
@@ -4,25 +4,17 @@ import com.example.dogdex.Dog
 import com.example.dogdex.R
 import com.example.dogdex.api.DogsApi.retrofitService
 import com.example.dogdex.api.dto.DogDTOMapper
+import com.example.dogdex.api.makeNetworkCall
 import com.example.dogdex.api.responses.ApiResponseStatus
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import java.net.UnknownHostException
 
 class DogRepository {
-    suspend fun downloadDogs():ApiResponseStatus<List<Dog>>{
-        return withContext(Dispatchers.IO){
-            try {
-                val dogListApiResponse = retrofitService.getAllDogs()
-                val dogDTOList = dogListApiResponse.data.dogs
-                val dogDTOMapper = DogDTOMapper()
-                ApiResponseStatus.Success(dogDTOMapper.fromDogDTOlistToDogDomainList(dogDTOList))
-            } catch (e: UnknownHostException){
-                ApiResponseStatus.Error(R.string.unknown_host_exception_error)
-
-            } catch (e: Exception){
-                ApiResponseStatus.Error(R.string.unknown_error)
-            }
-        }
+    suspend fun downloadDogs(): ApiResponseStatus<List<Dog>> = makeNetworkCall {
+        val dogListApiResponse = retrofitService.getAllDogs()
+        val dogDTOList = dogListApiResponse.data.dogs
+        val dogDTOMapper = DogDTOMapper()
+        dogDTOMapper.fromDogDTOlistToDogDomainList(dogDTOList)
     }
 }
