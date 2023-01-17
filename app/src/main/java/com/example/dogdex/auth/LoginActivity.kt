6ffe198bc1b0e04799472031a4fafd8b26579706
DavidMoralesAguilarUD <davidtrokten@gmail.com>
2023-01-16 23:25:57 -0500

    Navigating between fragments and creating Login and Sing Up Fragments

--- app/src/main/java/com/example/dogdex/auth/LoginActivity.kt
+++ app/src/main/java/com/example/dogdex/auth/LoginActivity.kt
@@ -2,11 +2,18 @@ package com.example.dogdex.auth
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
 import com.example.dogdex.R
+import com.example.dogdex.databinding.ActivityLoginBinding
 
-class LoginActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity(), LoginFragment.LoginFragmentActions {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        val binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    override fun onRegisterButtonClick() {
+        findNavController(R.id.nav_host_fragment).navigate(LoginFragmentDirections.actionLoginFragmentToSignUpFragment())
     }
 }
