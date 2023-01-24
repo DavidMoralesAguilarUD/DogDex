d0e6a5a16c56b8ce9d38d3ab4c4c41e5351ec6b1
DavidMoralesAguilarUD <davidtrokten@gmail.com>
2023-01-23 22:32:07 -0500

    Analizing images taken with CameraX

--- app/src/main/java/com/example/dogdex/MainActivity.kt
+++ app/src/main/java/com/example/dogdex/MainActivity.kt
@@ -6,14 +6,10 @@ import android.content.pm.PackageManager
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AlertDialog
-import androidx.camera.core.CameraSelector
-import androidx.camera.core.ImageCapture
-import androidx.camera.core.ImageCaptureException
-import androidx.camera.core.Preview
+import androidx.camera.core.*
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.content.ContextCompat
 import com.example.dogdex.api.ApiServiceInterceptor
@@ -23,7 +19,6 @@ import com.example.dogdex.doglist.DogListActivity
 import com.example.dogdex.model.User
 import com.example.dogdex.settings.SettingsActivity
 import java.io.File
-import java.util.concurrent.Executor
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 
@@ -187,9 +182,19 @@ class MainActivity : AppCompatActivity() {
 
             val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
 
+            val imageAnalysis = ImageAnalysis.Builder()
+                .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+                .build()
+            imageAnalysis.setAnalyzer(cameraExecutor) { imageProxy ->
+                val rotationDegrees = imageProxy.imageInfo.rotationDegrees
+
+                imageProxy.close()
+            }
+
+
             cameraProvider.bindToLifecycle(
                 this, cameraSelector,
-                preview, imageCapture
+                preview, imageCapture, imageAnalysis
             )
         }, ContextCompat.getMainExecutor(this))
 
